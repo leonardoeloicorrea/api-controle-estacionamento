ac12daea91ef3600d1232f6fba0db3885a89b67b
Leonardo correa <leonardoeloi24@gmail.com>
2024-02-25 15:34:16 -0300

    Criação da api e configs do postgre

---
+++ src/main/java/com/leonardo/apicontroleestacionamento/ApiControleEstacionamentoApplication.java
@@ -0,0 +1,13 @@
+package com.leonardo.apicontroleestacionamento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiControleEstacionamentoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiControleEstacionamentoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/leonardo/apicontroleestacionamento/ApiControleEstacionamentoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leonardo.apicontroleestacionamento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiControleEstacionamentoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
